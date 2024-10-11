73e5cb5bde24eff5463d830ddac6de8ebdb68521
Rohit Pawar <rohitpawar5062@gmail.com>
2024-10-11 23:00:34 +0530

    bug fixed once more

--- src/socket.jsx
+++ src/socket.jsx
@@ -6,7 +6,7 @@ const SocketContext=createContext()
 const getSocket =()=>useContext(SocketContext)
 
 const SocketProvider =({children})=>{
-    const socket =  useMemo(()=>io("http://localhost:3000",{withCredentials:true}),[])
+    const socket =  useMemo(()=>io("https://sandesh-backend-4ql2.onrender.com",{withCredentials:true}),[])
     return (
         <SocketContext.Provider value={socket}>
         {children}
